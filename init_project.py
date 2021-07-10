9e074ef533cd687862cd9de891e8abeb91079555
makamys <makamys@outlook.com>
2021-07-10 14:18:23 +0200

    Add mixin

--- init_project.py
+++ init_project.py
@@ -7,14 +7,18 @@ props = dict([x.strip().split("=") for x in list(open("gradle.properties", "r", 
 props["package"] = props["group"]
 props["mainclass"] = props["modid"].capitalize()
 
-template_path = "src/main/java/Template.java"
-source = Template(open(template_path, "r", encoding="utf8").read()).substitute(props)
+def apply_template(template_path, source_path_str):
+    source = Template(open(template_path, "r", encoding="utf8").read()).substitute(props)
 
-source_path = Path("src/main/java/" + props["package"].replace(".", "/") + "/" + props["mainclass"] + ".java")
+    source_path = Path(source_path_str)
 
-os.makedirs(source_path.parent, exist_ok=True)
-open(source_path, "w", encoding="utf8").write(source)
+    os.makedirs(source_path.parent, exist_ok=True)
+    open(source_path, "w", encoding="utf8").write(source)
+    
+    os.remove(template_path)
 
-os.remove(template_path)
+apply_template("src/main/java/Template.java", "src/main/java/" + props["package"].replace(".", "/") + "/" + props["mainclass"] + ".java")
+apply_template("src/main/resources/template.mixin.json", "src/main/resources/" + props["modid"] + ".mixin.json")
+
 os.remove(sys.argv[0])
 os.remove("README.md")
